18b7db4f2fa55a438f8a6c1feb9a1090f88fe6dd
mooreArrqs <amandanxumalo921@gmail.com>
2024-03-11 06:12:23 -0700

    prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# print alternating lowercase and uppercase ASCII
+# alphabets in reverse order.
+
+for i in range(ord('z'), ord('a') -1, -2):
+        print("{}{}".format(chr(i), chr(i - 33)), end='')
