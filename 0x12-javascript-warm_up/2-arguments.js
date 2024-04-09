d605513dfbeb0c3b834b6353d86fceec9f21a757
mooreArrqs <amandanxumalo921@gmail.com>
2024-04-09 02:55:59 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,12 +2,10 @@
 
 // a script that checks for a number of arguments passed.
 
-const Arg0 = "no_arg_are_passed";
-const Arg1 = "only_one_arg_passeg";
 
-if (no_arg_are_passed) {
+if (process.argv.length === 2) {
    console.log("No argument");
-} else if (only_one_arg_passed) {
+} else if (process.argv.length === 3) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
