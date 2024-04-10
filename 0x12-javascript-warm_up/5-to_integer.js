e2ba13dd1008a956c01822ef727e80528e0cae90
mooreArrqs <amandanxumalo921@gmail.com>
2024-04-10 02:26:46 -0700

    Looping over an array

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+if (first_arg_is_int) {
+  console.log("My number: ");
+} else {
+  console.log("Not a number");
+}
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// looping over an array
+
+for (const lines of ["C is fun", "Python is cool", "JavaScript is amazing"]) {
+  console.log(lines);
+}
