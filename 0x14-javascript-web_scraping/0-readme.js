1467354ec4631334d1c9c77c921e9145b7c5e791
mooreArrqs <amandanxumalo921@gmail.com>
2024-05-21 01:27:58 -0700

    Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+// import the built-in Node.js 'fs' module.
+
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+   // use fs.readFile() to read the contents of a file specified as a command-line argument
+   // 'utf8' specifies the encoding of the file read
+
+   if (error) {
+     // If an error occurs when reading the file, the 'error' parameter will contain an error object.
+     console.error('Error reading the file:', error);
+   } else {
+     // If the file reading was a success, the 'content' parameter will contain the contents of the file as a string.
+     console.log(content);
+   }
+});
