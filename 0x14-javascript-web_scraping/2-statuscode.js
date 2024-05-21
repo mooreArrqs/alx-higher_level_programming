009a3ea185b69049d4ba089ccf8124a26a044766
mooreArrqs <amandanxumalo921@gmail.com>
2024-05-21 11:34:07 -0700

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,9 +6,9 @@ const request = require('request');
 request.get(process.argv[2])
 // Use the 'request' module to perform an HTTP GET request to the URL.
 
-   .on('response', function (response) {
-     // Set up an event listener for the 'response' event emitted by the HTTP request.
+  .on('response', function (response) {
+    // Set up an event listener for the 'response' event emitted by the HTTP request.
 
-     console.log(`code: ${response.statusCode}`);
-     // Log the HTTP status code of the response to the console.
-   });
+    console.log(`code: ${response.statusCode}`);
+  // Log the HTTP status code of the response to the console.
+  });
